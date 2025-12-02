22939cce9c056b8e5c7e914508982268c1a35536
itzjaymzz <jamesisquick@gmail.com>
2025-12-02 08:15:23 -0700

    add cpp file

---
+++ project.cpp
@@ -0,0 +1,10 @@
+//James Cole & Isaac Sipma
+//December 02, 2025
+//Password strength checker, it checks your password and checks it on 
+//different criteria to see if its a good password or whether you should chnage it
+
+
+int main() {
+
+	return 0;
+}
